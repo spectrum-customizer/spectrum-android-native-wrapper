b204e93c2af593cfca58a2ba11a685159db346ba
TomFalkner <tafalkner@gmail.com>
2020-01-31 14:31:59 -0800

    Cleanup tests, configure bintray

--- spectrumcustomizer/src/test/java/com/spectrumcustomizer/integration/AddToCartEventArgsTest.java
+++ spectrumcustomizer/src/test/java/com/spectrumcustomizer/integration/AddToCartEventArgsTest.java
@@ -8,17 +8,10 @@ import org.junit.Test;
 public class AddToCartEventArgsTest {
 
     SpectrumAddToCartEventArgs args;
-    String recipeSetReadableId = "ABCD1234";
 
     @Before
     public void setup() {
         args = new SpectrumAddToCartEventArgs();
-    }
-
-    @Test
-    public void hasRecipeSetId() {
-        args.recipeSetReadableId = recipeSetReadableId;
-        assertEquals(recipeSetReadableId, args.recipeSetReadableId);
     }
 
 }
